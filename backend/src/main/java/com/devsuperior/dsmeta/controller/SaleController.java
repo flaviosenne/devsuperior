5c62822ca2c29ed6f7bee95bae9b31afe1805f20
flaviosenne <flaviosenne123@gmail.com>
2022-09-07 09:49:38 -0300

    criar service, repository e controller do sales

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dsmeta.controller;
+
+import com.devsuperior.dsmeta.entity.Sale;
+import com.devsuperior.dsmeta.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+    @Autowired
+    private SaleService service;
+
+    @GetMapping
+    public ResponseEntity<List<Sale>> findSales(){
+        return ResponseEntity.ok(service.findSales());
+    }
+}
