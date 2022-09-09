800736baeed9a325484050d659e43f5c402882af
flaviosenne <flaviosenne123@gmail.com>
2022-09-09 13:33:42 -0300

    notificação implementada

--- frontend/src/components/notification-button/index.tsx
+++ frontend/src/components/notification-button/index.tsx
@@ -1,9 +1,24 @@
 import icon from '../../assets/img/notification-icon.svg'
 import './styles.css'
+import axios from 'axios'
+import { BASE_URL } from '../../utils/request'
+import {toast} from 'react-toastify'
 
-function NotificationButton(){
+type Props = {
+    saleId: number
+}
+
+function NotificationButton({saleId}: Props){
+    
+    function handleClick(id: number){
+        axios.get(`${BASE_URL}/sales/${id}/notification`)
+        .then(res => {
+            toast.info('SMS enviado com sucesso')
+        })
+    }
+    
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={()=> handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
